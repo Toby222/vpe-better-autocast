ea340e50e7288ee660d9550d72f6e0232284b013
Tobias Berger <toby@tobot.dev>
2024-03-26 21:38:47 +0100

    Update launch script

--- .vscode/launch.sh
+++ .vscode/launch.sh
@@ -4,6 +4,6 @@ script_dir=$(dirname $(readlink -f $0))
 mod_dir=$(dirname $script_dir)
 pushd $script_dir
 
-SDL_VIDEODRIVER=X11 LC_ALL=C steam-run ~/.steam/steam/steamapps/common/RimWorld/RimWorldLinux -logfile $mod_dir/log/rimworld.log -popupwindow $@
+SDL_VIDEODRIVER=X11 LC_ALL=C steam-run ~/.local/share/Steam/steamapps/common/RimWorld/RimWorldLinux -logfile $mod_dir/log/rimworld.log -popupwindow $@
 
 popd
