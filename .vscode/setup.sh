1bf5e503cfba85030a1b5e92a2f2e3e9f0ef68dd
Tobias Berger <toby@tobot.dev>
2024-03-27 22:49:35 +0100

    Fix task order

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -1,4 +1,5 @@
 set -e
+set -x
 
 script_dir=$(dirname $(readlink -f $0))
 mod_dir=$(dirname $script_dir)
@@ -11,7 +12,7 @@ if [ "$target_dir" = "$mod_dir" ]; then
     echo "Target directory is the same as the mod directory. Cloned in RimWorld/Mods. Skipping setup"
 else
     mkdir -p $target_dir
-    rsync -av --exclude '$mod_dir/*' --delete $mod_dir/1.4 $mod_dir/About $mod_dir/README $mod_dir/LICENSE  $target_dir
+    rsync -av --exclude '$mod_dir/*' --delete $mod_dir/1.4 $mod_dir/About $mod_dir/README $mod_dir/LICENSE $target_dir
 fi
 
 popd
