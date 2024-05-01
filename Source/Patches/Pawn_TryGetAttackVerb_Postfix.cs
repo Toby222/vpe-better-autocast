a6ca47f328edaac3ffe32771df070ede06f77195
Tobias Berger <toby@tobot.dev>
2024-05-01 15:09:18 +0200

    Don't check for null Verb in TryGetAttackVerb Patch
    If the previous patches returned null, simply
    pass that along.
    
    Also don't use ToList.
    
    Fixes #1

--- Source/Patches/Pawn_TryGetAttackVerb_Postfix.cs
+++ Source/Patches/Pawn_TryGetAttackVerb_Postfix.cs
@@ -17,36 +17,22 @@ internal static class Pawn_TryGetAttackVerb_Postfix
     {
         if (__instance is null)
             throw new ArgumentNullException(nameof(__instance));
-        if (__result is null)
-            throw new ArgumentNullException(nameof(__result));
 
         if (__instance.TryGetComp<CompAbilities>() is not CompAbilities compAbilities)
             return;
 
-        List<Verb_CastAbility> list = compAbilities
+        IEnumerable<Verb_CastAbility> castableVerbs = compAbilities
             .LearnedAbilities.Where(ability =>
                 ability.autoCast
                 && !AbilityIsBlacklisted(ability)
                 && ability.IsEnabledForPawn(out string reason)
                 && (target == null || ability.CanHitTarget(target))
             )
-            .Select(ability => ability.verb)
-            .ToList();
-        // List<Verb_CastAbility> list = (
-        //     from ab in compAbilities.LearnedAbilities
-        //     where
-        //         ab.Autocast
-        //         && !AbilityIsBlacklisted(ab)
-        //         && ab.IsEnabledForPawn(out reason)
-        //         && (target == null || ab.CanHitTarget(target))
-        //     select ab.verb
-        // ).ToList();
-        if (list.Count == 0)
-            return;
+            .Select(ability => ability.verb);
 
         if (target is not null)
         {
-            var result = list.Where(x => x.ability.AICanUseOn(target))
+            var result = castableVerbs.Where(x => x.ability.AICanUseOn(target))
                 .Select(x => new Tuple<Verb, float>(x, x.ability.Chance))
                 .AddItem(new Tuple<Verb, float>(__result, 1f))
                 .GetRandomClass(t => t.Item2);
@@ -58,7 +44,7 @@ internal static class Pawn_TryGetAttackVerb_Postfix
         }
         else
         {
-            __result = list.AddItem(__result).MaxBy((Verb ve) => ve.verbProps.range);
+            __result = castableVerbs.AddItem(__result).MaxBy((Verb ve) => ve.verbProps.range);
         }
     }
 
