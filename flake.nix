3661a08c2456a7cd533fdf0f4a8fdc9e45d6bb20
Tobias Berger <toby@tobot.dev>
2024-08-10 10:00:03 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     # Dev tools
