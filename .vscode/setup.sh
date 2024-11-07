319c004cc2db8c0db27129457d4140c95d2cb958
Tobias Berger <toby@tobot.dev>
2024-11-07 19:48:35 +0100

    Fix LoadFolders

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -12,7 +12,7 @@ if [ "$target_dir" = "$mod_dir" ]; then
   echo "Target directory is the same as the mod directory. Cloned in RimWorld/Mods. Skipping setup"
 else
   mkdir -p $target_dir
-  rsync -av --exclude '$mod_dir/*' --delete $mod_dir/1.4 $mod_dir/1.5 $mod_dir/Patches $mod_dir/About $mod_dir/README $mod_dir/LICENSE $mod_dir/Changelog.md $target_dir
+  rsync -av --exclude '$mod_dir/*' --delete $mod_dir/1.4 $mod_dir/1.5 $mod_dir/Compatibility $mod_dir/LoadFolders.xml $mod_dir/About $mod_dir/README $mod_dir/LICENSE $mod_dir/Changelog.md $target_dir
 fi
 
 popd
